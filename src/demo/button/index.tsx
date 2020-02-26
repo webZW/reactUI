51a2da6d49e446f9a874e8e74123f2d0049e2600
zw_web <313921175@qq.com>
2020-02-27 00:53:06 +0800

    按钮-不可选样式

--- src/demo/button/index.tsx
+++ src/demo/button/index.tsx
@@ -51,18 +51,8 @@ const Button = () => {
                      {/* <Icon type="right" /> */}
                   </ComButton>
                </ComButton.Group>
-               {/* <ComButton className="mr15" type="primary" onClick={ () => setSize('large') }>large</ComButton>
-               <ComButton className="mr15" type="primary" onClick={ () => setSize('default') }>default</ComButton>
-               <ComButton className="mr15" type="primary" onClick={ () => setSize('small') }>small</ComButton> */}
             </div>
             <br />
-            {/* <Radio.Group value={size} onChange={this.handleSizeChange}>
-               <Radio.Button value="large">Large</Radio.Button>
-               <Radio.Button value="default">Default</Radio.Button>
-               <Radio.Button value="small">Small</Radio.Button>
-            </Radio.Group>
-            <br />
-            <br /> */}
             <ComButton className="mr15" type="primary" size={size}>Primary </ComButton>
             <ComButton className="mr15" size={size}>Normal</ComButton>
             <ComButton className="mr15" type="dashed" size={size}> Dashed </ComButton>
@@ -75,6 +65,45 @@ const Button = () => {
             <ComButton className="mr15" type="primary" shape="round" icon="download" size={size} />
             <ComButton className="mr15" type="primary" shape="round" icon="download" size={size}> Download </ComButton>
             <ComButton className="mr15" type="primary" icon="download" size={size}> Download </ComButton>
+         </div>
+
+         <div className="pb15">
+            <p>按钮组合</p>
+            <h4>Basic</h4>
+            <ComButton.Group className="mr15">
+               <ComButton>Cancel</ComButton>
+               <ComButton>OK</ComButton>
+            </ComButton.Group>
+            <ComButton.Group className="mr15">
+               <ComButton disabled>L</ComButton>
+               <ComButton disabled>M</ComButton>
+               <ComButton disabled>R</ComButton>
+            </ComButton.Group>
+            <ComButton.Group className="mr15">
+               <ComButton>L</ComButton>
+               <ComButton>M</ComButton>
+               <ComButton>R</ComButton>
+            </ComButton.Group>
+
+            <h4>With Icon</h4>
+            <ComButton.Group className="mr15">
+               <ComButton type="primary">
+               {/* <Icon type="left" /> */}
+               Go back
+               </ComButton>
+               <ComButton type="primary" className="mr15">
+               Go forward
+               {/* <Icon type="right" /> */}
+               </ComButton>
+            </ComButton.Group>
+            <ComButton.Group className="mr15">
+               <ComButton type="primary" icon="cloud" />
+               <ComButton type="primary" icon="cloud-download" />
+            </ComButton.Group>
+            <ComButton.Group className="mr15">
+               <ComButton type="primary" size="small" icon="cloud" />
+               <ComButton type="primary" size="small" icon="cloud-download" />
+            </ComButton.Group>
          </div>
       </div>
    )
